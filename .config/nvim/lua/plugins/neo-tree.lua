4e2165602d9954937ff2cd50e1924173dd89db9e
Marco Kutscha <kutscha.marco@gmail.com>
2024-02-18 22:37:17 +0100

    Neotree winbar

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,16 +1,18 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-  },
-  config=function()
-    require('neo-tree').setup({
-      follow_current_file = true
-    })
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem float<CR>', {})
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			follow_current_file = true,
+			source_selector = {
+				winbar = true,
+      },
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem float<CR>", {})
+	end,
 }
-
