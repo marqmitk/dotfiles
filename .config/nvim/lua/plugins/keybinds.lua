8fa6ba8afd2957e12b7c5683800eebdd47b2a3f2
Marco Kutscha <kutscha.marco@gmail.com>
2024-02-22 12:37:37 +0100

    now with auto save and working preview

--- .config/nvim/lua/plugins/keybinds.lua
+++ .config/nvim/lua/plugins/keybinds.lua
@@ -1,73 +1,77 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	config = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 200
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 200
 
-		local wk = require("which-key")
+    local wk = require("which-key")
 
-		mappings = {
-			["<leader>"] = {
-				f = {
-					name = "+Find",
-					f = { "Find File" },
-					r = { "Open Recent File" },
-					g = { "Grep" },
-					b = { "Buffers" },
-					d = { "Jump to Definition" },
-				},
+    mappings = {
+      ["<leader>"] = {
+        f = {
+          name = "+Find",
+          f = { "Find File" },
+          r = { "Open Recent File" },
+          g = { "Grep" },
+          b = { "Buffers" },
+          d = { "Jump to Definition" },
+        },
 
-				g = {
-					name = "+Git",
-					g = { "<cmd>Telescope git_status<cr>", "Git Status" },
-					b = { "<cmd>Telescope git_branches<cr>", "Checkout Branch" },
-					c = { "<cmd>Telescope git_commits<cr>", "Checkout Commit" },
-					s = { "<cmd>Telescope git_stash<cr>", "Stash" },
-				},
+        g = {
+          name = "+Git",
+          g = { "<cmd>Telescope git_status<cr>", "Git Status" },
+          b = { "<cmd>Telescope git_branches<cr>", "Checkout Branch" },
+          c = { "<cmd>Telescope git_commits<cr>", "Checkout Commit" },
+          s = { "<cmd>Telescope git_stash<cr>", "Stash" },
+        },
 
-				d = {
-					name = "+Debug",
-					t = { "Toggle Breakpoint" },
-					o = { "Step Over" },
-					i = { "Step Into" },
-					s = { "Step Out" },
-					c = { "Continue" },
-				},
+        d = {
+          name = "+Debug",
+          t = { "Toggle Breakpoint" },
+          o = { "Step Over" },
+          i = { "Step Into" },
+          s = { "Step Out" },
+          c = { "Continue" },
+        },
 
-				b = {
-					name = "+Bar",
-					m = { "Next Tab" },
-					n = { "Previous Tab" },
-					["1"] = { "Jump to Tab 1" },
-					["2"] = { "Jump to Tab 2" },
-					["3"] = { "Jump to Tab 3" },
-					["4"] = { "Jump to Tab 4" },
-				},
+        b = {
+          name = "+Bar",
+          m = { "Next Tab" },
+          n = { "Previous Tab" },
+          ["1"] = { "Jump to Tab 1" },
+          ["2"] = { "Jump to Tab 2" },
+          ["3"] = { "Jump to Tab 3" },
+          ["4"] = { "Jump to Tab 4" },
+        },
 
-				c = {
-					name = "+Code",
-					a = { "Code Action" },
-				},
+        c = {
+          name = "+Code",
+          a = { "Code Action" },
+        },
 
-				o = {
-					name = "+Open",
-					o = { "Outline" },
-					u = { "Undo Tree" },
-				},
-				s = {
-					name = "+Split",
-					v = { "Vertical Split" },
-					s = { "Horizontal Split" },
-				},
-				h = {
-					name = "+Harpoon",
-					a = { "Add file harpoon list" },
-					m = { "Open harpoon menu" },
-				},
-			},
-		}
+        o = {
+          name = "+Open",
+          o = { "Outline" },
+          u = { "Undo Tree" },
+        },
+        s = {
+          name = "+Split",
+          v = { "Vertical Split" },
+          s = { "Horizontal Split" },
+        },
+        h = {
+          name = "+Harpoon",
+          a = { "Add file harpoon list" },
+          m = { "Open harpoon menu" },
+        },
+        p = {
+          name = "+Preview",
+          l = { "Start Latex Preview" },
+        },
+      },
+    }
 
-		wk.register(mappings, {})
-	end,
+    wk.register(mappings, {})
+  end,
 }
--- .config/nvim/lua/plugins/latex.lua
+++ .config/nvim/lua/plugins/latex.lua
@@ -1,4 +1,7 @@
 return {
   "xuhdev/vim-latex-live-preview",
-  config = function() end,
+  config=function ()
+    -- create a keybind for :LLPStartPreview on the button <leader>pl
+    vim.api.nvim_set_keymap("n", "<leader>pl", ":LLPStartPreview<CR>", { noremap = true, silent = true })
+  end
 }
