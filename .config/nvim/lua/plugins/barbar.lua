91786a507196c3ef4b379d2d8fc91a4e65afc8ea
Marco Kutscha <kutscha@student.tugraz.at>
2024-03-27 13:46:31 +0100

    fix

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -13,7 +13,7 @@ return {
     },
     config = function()
       vim.g.barbar_auto_setup = false
-      vim.keymap.set('n', '<leader>b1', '<cmd>BufferGoto 1<CR>') 
+      vim.keymap.set('n', '<leader>b1', '<cmd>BufferGoto 1<CR>')
       vim.keymap.set('n', '<leader>b2', '<cmd>BufferGoto 2<CR>')
       vim.keymap.set('n', '<leader>b3', '<cmd>BufferGoto 3<CR>')
       vim.keymap.set('n', '<leader>b4', '<cmd>BufferGoto 4<CR>')
